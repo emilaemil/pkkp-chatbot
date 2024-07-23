e4a7ff4cbc9c2a49d04fd1b078bd4a82ecf7554e
Emila <emilaaprilia30@gmail.com>
2024-07-23 14:34:44 +0700

    change file app.py

--- app.py
+++ app.py
@@ -71,4 +71,5 @@ def chatbot():
     return jsonify({'responses': response})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
